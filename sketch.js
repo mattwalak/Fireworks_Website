9b2d02ad9d34282bbf5bfd16c80636780c35fc88
Loaf! <walakmatthew@gmail.com>
2022-09-22 10:32:16 -0400

    update

--- sketch.js
+++ sketch.js
@@ -1,31 +1,13 @@
-
-let t = 0; // time variable
-
+let button;
 function setup() {
-  createCanvas(600, 600);
-  noStroke();
-  fill(40, 200, 40);
+  createCanvas(1000, 1000);
+  background(0);
+  button = createButton('click me');
+  button.position(0, 0);
+  button.mousePressed(changeBG);
 }
 
-function draw() {
-  background(10, 10); // translucent background (creates trails)
-
-  // make a x and y grid of ellipses
-  for (let x = 0; x <= width; x = x + 30) {
-    for (let y = 0; y <= height; y = y + 30) {
-      // starting point of each circle depends on mouse position
-      const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
-      const yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
-      // and also varies based on the particle's location
-      const angle = xAngle * (x / width) + yAngle * (y / height);
-
-      // each particle moves in a circle
-      const myX = x + 20 * cos(2 * PI * t + angle);
-      const myY = y + 20 * sin(2 * PI * t + angle);
-
-      ellipse(myX, myY, 10); // draw particle
-    }
-  }
-
-  t = t + 0.01; // update time
+function changeBG() {
+  let val = random(255);
+  background(val);
 }
