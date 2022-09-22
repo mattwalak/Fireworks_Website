07efc5eda2338557c365955ecb2b461ece87688f
Loaf! <walakmatthew@gmail.com>
2022-09-22 11:03:02 -0400

    js time

--- sketch.js
+++ sketch.js
@@ -11,3 +11,32 @@ function changeBG() {
   let val = random(255);
   background(val);
 }
+
+// SERVER THINGS
+
+
+let socket = new WebSocket("wss://54.147.44.11:42742");
+
+socket.onopen = function(e) {
+  alert("[open] Connection established");
+  alert("Sending to server");
+  socket.send("My name is John");
+};
+
+socket.onmessage = function(event) {
+  alert(`[message] Data received from server: ${event.data}`);
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  } else {
+    // e.g. server process killed or network down
+    // event.code is usually 1006 in this case
+    alert('[close] Connection died');
+  }
+};
+
+socket.onerror = function(error) {
+  alert(`[error] ${error.message}`);
+};
